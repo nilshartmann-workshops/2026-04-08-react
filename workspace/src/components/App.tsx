de6370de24d6bd4c4d0bb0864c64bc128baed11b
Nils Hartmann <nils@nilshartmann.net>
2026-04-08 11:09:34 +0200

    Revert "Mehrere Varianten, die TabBar zu strukturieren"
    This reverts commit d7322f1b6881708aa73768923cdcaf9c92088c7c.

--- workspace/src/components/App.tsx
+++ workspace/src/components/App.tsx
@@ -1,25 +1,14 @@
 import { Panel, Tab, TabBar } from "./TabBar.tsx";
 import PlantList from "./PlantList.tsx";
 import PlantForm from "./PlantForm.tsx";
-import { ReactNode, useState } from "react";
-
-type TabDescription = Array<{
-  title: string;
-  component: ReactNode
-}>
+import { useState } from "react";
 
 export default function App() {
   const [tabId, setTabId] = useState("liste");
-
-  const tabDescription = [
-    {title: "Pflanzen", icon: <Icon />,  component: <PlantList />},
-    {title: "Pflanze hinzufügen", component: <PlantForm />},
-  ];
-
   return (
     <div className={"AppContainer"}>
       <TabBar>
-        <Tab tabId="liste" activeTabId={tabId} onTabChange={(tabId) => setTabId(tabId)}>Pflanzen Liste </Tab>
+        <Tab tabId="liste" activeTabId={tabId} onTabChange={(tabId) => setTabId(tabId)}>Pflanzen Liste</Tab>
         <Tab tabId="form" activeTabId={tabId} onTabChange={(tabId) => setTabId(tabId)}>Pflanze anlegen</Tab>
 
         <Panel tabId="liste" activeTabId={tabId}>
@@ -32,17 +21,4 @@ export default function App() {
       </TabBar>
     </div>
   );
-}
-
-type TabBarNeuProps = {
-  descriptions: TabDescription[]
-}
-
-function DefaultTabBar({descriptions}: TabBarNeuProps) {
-
-  <TabBar>
-    {descriptions.map(x => <Tab />
-    {descriptions.map(x => <Panel />
-  </TabBar>
-
 }
