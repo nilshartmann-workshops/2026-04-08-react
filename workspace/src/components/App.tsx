d7322f1b6881708aa73768923cdcaf9c92088c7c
Nils Hartmann <nils@nilshartmann.net>
2026-04-08 11:09:24 +0200

    Mehrere Varianten, die TabBar zu strukturieren

--- workspace/src/components/App.tsx
+++ workspace/src/components/App.tsx
@@ -1,14 +1,25 @@
 import { Panel, Tab, TabBar } from "./TabBar.tsx";
 import PlantList from "./PlantList.tsx";
 import PlantForm from "./PlantForm.tsx";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
+
+type TabDescription = Array<{
+  title: string;
+  component: ReactNode
+}>
 
 export default function App() {
   const [tabId, setTabId] = useState("liste");
+
+  const tabDescription = [
+    {title: "Pflanzen", icon: <Icon />,  component: <PlantList />},
+    {title: "Pflanze hinzufügen", component: <PlantForm />},
+  ];
+
   return (
     <div className={"AppContainer"}>
       <TabBar>
-        <Tab tabId="liste" activeTabId={tabId} onTabChange={(tabId) => setTabId(tabId)}>Pflanzen Liste</Tab>
+        <Tab tabId="liste" activeTabId={tabId} onTabChange={(tabId) => setTabId(tabId)}>Pflanzen Liste </Tab>
         <Tab tabId="form" activeTabId={tabId} onTabChange={(tabId) => setTabId(tabId)}>Pflanze anlegen</Tab>
 
         <Panel tabId="liste" activeTabId={tabId}>
@@ -21,4 +32,17 @@ export default function App() {
       </TabBar>
     </div>
   );
+}
+
+type TabBarNeuProps = {
+  descriptions: TabDescription[]
+}
+
+function DefaultTabBar({descriptions}: TabBarNeuProps) {
+
+  <TabBar>
+    {descriptions.map(x => <Tab />
+    {descriptions.map(x => <Panel />
+  </TabBar>
+
 }
